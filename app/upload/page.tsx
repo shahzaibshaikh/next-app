ca4e98ede3cb135d877cda5c89de9727c63a2202
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2024-02-06 18:30:10 -0500

    👌 IMPROVE: integrated CldImage

---
+++ app/upload/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+import React, { useState } from "react";
+import { CldImage, CldUploadWidget } from "next-cloudinary";
+
+interface CloudinaryResult {
+  public_id: string;
+}
+
+const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
+
+  return (
+    <div>
+      {publicId && <CldImage src={publicId} width={270} height={180} alt='A coffee' />}
+      <CldUploadWidget
+        uploadPreset='u1lnura1'
+        onUpload={(result, widget) => {
+          if (result.event != "success") return;
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => (
+          <button className='btn btn-primary' onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </div>
+  );
+};
+
+export default UploadPage;
