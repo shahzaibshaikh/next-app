17e8d99cbcaea7213e766c6a2ebba7e475913e27
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2024-02-06 16:01:49 -0500

    👌 IMPROVE: added db integration to APIs

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,23 +1,43 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "../schema";
+import prisma from "@/prisma/client";
 
-export function GET(request: NextRequest, { params }: { params: { id: number } }) {
-  if (params.id > 10)
-    return NextResponse.json({ error: "User not found" }, { status: 404 });
-  return NextResponse.json({ id: 1, name: "Mosh" });
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+  // Convert params.id to a number
+  const userId = parseInt(params.id);
+
+  const user = await prisma.user.findUnique({
+    where: { id: userId }
+  });
+
+  if (!user) return NextResponse.json({ error: "User not found" }, { status: 404 });
+  return NextResponse.json(user);
 }
 
-export async function PUT(request: NextRequest, { params }: { params: { id: number } }) {
+export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
   const body = await request.json();
   const validation = schema.safeParse(body);
 
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
 
-  if (params.id > 10)
-    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  const user = await prisma.user.findUnique({
+    where: {
+      id: parseInt(params.id)
+    }
+  });
 
-  return NextResponse.json({ id: 1, name: body.name });
+  if (!user) return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  const updatedUser = await prisma.user.update({
+    where: { id: user.id },
+    data: {
+      name: body.name,
+      email: body.email
+    }
+  });
+
+  return NextResponse.json(updatedUser);
 }
 
 export async function DELETE(
