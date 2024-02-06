775b7a9e2ce91f23cc9685c909f1d354cfaa819b
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2024-02-06 17:16:32 -0500

    👌 IMPROVE: added products API

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,12 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "@/prisma/client";
 
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: "Milk", price: 5 },
-    { id: 2, name: "Eggs", price: 2 },
-    { id: 3, name: "Bread", price: 4 }
-  ]);
+export async function GET(request: NextRequest) {
+  const products = await prisma.product.findMany();
+  return NextResponse.json(products);
 }
 
 export async function POST(request: NextRequest) {
@@ -16,5 +14,12 @@ export async function POST(request: NextRequest) {
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
 
-  return NextResponse.json([{ id: 10, name: body.name, price: body.price }]);
+  const newProduct = await prisma.product.create({
+    data: {
+      name: body.name,
+      price: body.price
+    }
+  });
+
+  return NextResponse.json(newProduct);
 }
