ffa0df820e4e12ac4b746b5b8004f7491f7e5e62
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2024-02-06 20:02:55 -0500

    👌 IMPROVE: added next-auth

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -14,6 +14,11 @@ const UploadPage = () => {
       {publicId && <CldImage src={publicId} width={270} height={180} alt='A coffee' />}
       <CldUploadWidget
         uploadPreset='u1lnura1'
+        options={{
+          sources: ["local"],
+          multiple: false,
+          maxFiles: 5
+        }}
         onUpload={(result, widget) => {
           if (result.event != "success") return;
           const info = result.info as CloudinaryResult;
